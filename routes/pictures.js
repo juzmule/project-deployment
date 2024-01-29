8f5950adbbed8fe3e0c4ded7c155dc911b3bb83c
juzmule <stian.lundberg96@gmail.com>
2024-01-29 12:13:42 +0100

    Saving pictures

---
+++ routes/pictures.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+const fs = require('fs');
+var path = require('path');
+
+// Or
+/* GET pictures listing. */
+router.get('/', function(req, res, next) {
+  const pictures = fs.readdirSync(path.join(__dirname, '../pictures/'));
+  res.render('pictures', { pictures: pictures});
+});
+
+router.post('/', function(req, res, next) {
+  const file = req.files.file;
+  fs.writeFileSync(path.join(__dirname, '../pictures/', file.name), file.data);
+  res.end();
+});
+
+module.exports = router;
