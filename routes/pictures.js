4bc5cef32dc36fc1cb983f6eea62b4d1c29754d2
juzmule <stian.lundberg96@gmail.com>
2024-01-30 12:57:26 +0100

    pictureDetails update

--- routes/pictures.js
+++ routes/pictures.js
@@ -29,9 +29,18 @@ router.get('/', requiresAuth(), async function(req, res, next) {
   res.render('pictures', { pictures: pictures});
 });
 
-router.get('/:pictureName', requiresAuth(), async function(req, res, next) {
-  res.render('pictureDetails', { picture: req.params.pictureName});
-});
+router.get('/:pictureName', requiresAuth(), async function(req, res,
+  next) {
+  let my_file = await s3.getObject({
+    Bucket: process.env.CYCLIC_BUCKET_NAME,
+    Key: "public/" + req.params.pictureName,
+   }).promise();
+  const picture = {
+     src: Buffer.from(my_file.Body).toString('base64'),
+     name: req.params.pictureName
+   }
+  res.render('pictureDetails', { picture: picture});
+  });
 
 router.post('/', requiresAuth(), async function(req, res, next) {
   const file = req.files.file;
